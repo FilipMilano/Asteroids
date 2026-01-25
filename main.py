7111083feb195ffd8e572c082b3a6105f484b77a
FilipMilano <filipmilano@outlook.com>
2026-01-25 19:47:54 +0100

    clock med fps

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -19,7 +20,8 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
-    
+        dt = clock.tick(60) / 1000
+        
 
 
 
