993e8a3e2aea7959b1cc303e0df5600a59eb0ce2
FilipMilano <filipmilano@outlook.com>
2026-01-26 04:24:27 +0100

    Moving asteroids

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -11,18 +13,30 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
+    
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    AsteroidField.containers = (updatable,)
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    AsteroidField()
     while True:
         log_state()
 
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
-        
-        player.update(dt)
+
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+        
+        for graphic in drawable:
+             graphic.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
